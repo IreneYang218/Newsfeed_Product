c2c420c9491d49d1b7cf2fe153ac58d33725b6a9
crystalxs <xsun45@dons.usfca.edu>
2019-03-28 16:49:44 -0700

    non-layout demo with webhose.io data

---
+++ frontend/code.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// some important global variables.
+// the data source
+const API_SERVER = 'https://webhose.io/filterWebContent?token=6caf20d8-8bad-49fb-996e-0726d3621783';
+const QUERY = '&sort=crawled&q=site_type%3Anews%20thread.country%3AUS%20language%3Aenglish';
+const FORMAT = '&format=json';
+const COMMENT_VIEW = 'connectsf_comment';
+
+// main function
+let _featJson;
+async function initialPrep() {
+
+  console.log('1...');
+  _featJson = await getNews();
+
+  console.log('2... ');
+  console.log("data", app_news.news);
+  
+  console.log('3... ');
+  
+  
+  console.log('4... ');
+  // await fetchAddLayers();
+
+  console.log('5... ');
+  // await checkCookie();
+
+  console.log('6 !!!');
+}
+
+// get data from RESTapi
+async function getNews() {
+  let data_url = API_SERVER + FORMAT + QUERY;
+  let resp = await fetch(data_url);
+  let jsonData = await resp.json();
+
+  app_news.news = jsonData['posts'];
+}
+
+var app_news = new Vue({
+  el: '#container',
+  data: {
+    todos: [
+      { text: "What is our app's name?" },
+      { text: 'Do you like this layout?' },
+      { text: 'Get news from api' },
+      { text: 'Link news to their origin website.' },
+      { text: 'User login entrance.' },
+      { text: 'Todo' },
+      { text: 'Todo' },
+      { text: 'Todo' },
+    ],
+    news: []
+  }
+})
+
+initialPrep();
