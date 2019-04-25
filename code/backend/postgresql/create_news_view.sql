e224ef84daeef9b99bc1331dab01cf130f443fff
crystalxs <xsun45@dons.usfca.edu>
2019-04-25 15:14:01 -0700

    add restful api

--- code/backend/postgresql/create_news_view.sql
+++ code/backend/postgresql/create_news_view.sql
@@ -6,4 +6,6 @@ CREATE OR REPLACE VIEW api.articles AS
     SELECT *
     FROM newsphi.news_articles;
 
-grant select on api.articles to web_anon;
+ALTER TABLE api.articles OWNER TO newsphi;
+grant ALL on TABLE api.articles to newsphi;
+grant select on TABLE api.articles to web_anon;
