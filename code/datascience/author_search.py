114965c179f9537b24b863955f8ce52f099340ec
IreneYang218 <ireneyang218@gmail.com>
2019-05-06 00:10:48 -0700

    author_twitter_added

---
+++ code/datascience/author_search.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import tweepy
+from author_user_definition import *
+
+
+def loadkeys(filename):
+    """"
+    load twitter api keys/tokens from CSV file with form
+    consumer_key, consumer_secret, access_token, access_token_secret
+    """
+    with open(filename) as f:
+        items = f.readline().strip().split(',')
+        return items
+
+
+def authenticate(twitter_auth_filename):
+    """
+    Given a file name containing the Twitter keys and tokens,
+    create and return a tweepy API object.
+    """
+    consumer_key, consumer_secret, \
+        access_token, access_token_secret = loadkeys(twitter_auth_filename)
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(
+        auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    return api
+
+
+if __name__ == '__main__':
+    df = pd.read_csv(data_file)
+    df_author = df['author'].drop_duplicates()
+    df_author = df_author[~df_author.isnull()]
+    api = authenticate(token_file)
+    author_list = df_author.tolist()
+    f = open(author_search, "w")
+    f.write('idx,author,author_screen_name\n')
+    f.close()
+    for i, a in enumerate(author_list):
+        if i % 500 == 0:
+            print(str(i) + ' ' + 'author scraped')
+        if ',' in str(a):
+            a = str(a).split(',')[0]
+        if (isinstance(a, str) and len(str(a).split(' '))) > 10:
+            continue
+        else:
+            search = api.search_users(a)
+        try:
+            s_name = search[0].screen_name
+        except IndexError:
+            s_name = '\n'
+        f = open(author_search, "a")
+        f.write(str(i))
+        f.write(',')
+        f.write(str(a))
+        f.write(',')
+        f.write(str(s_name))
+        f.write('\n')
+        f.close()
---
+++ code/datascience/author_twitter.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import tweepy
+import numpy as np
+from author_user_definition import *
+
+
+def loadkeys(filename):
+    """"
+    load twitter api keys/tokens from CSV file with form
+    consumer_key, consumer_secret, access_token, access_token_secret
+    """
+    with open(filename) as f:
+        items = f.readline().strip().split(',')
+        return items
+
+
+def authenticate(twitter_auth_filename):
+    """
+    Given a file name containing the Twitter keys and tokens,
+    create and return a tweepy API object.
+    """
+    consumer_key, consumer_secret, \
+        access_token, access_token_secret = loadkeys(twitter_auth_filename)
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(
+        auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    return api
+
+
+def fetch_user(api, con_dict):
+    """
+    Given a tweepy API object and the con_dict of
+    the authors' Twitter accounts,
+    append authors' twitter information in the original dictionary.
+    The key-values pairs contain:
+        followers_count: The number of followers this account currently has.
+        listed_count: The number of public lists that
+            this user is a member of a list
+        verified: Whether the Twitter account is verified or not
+    :param api: twitter api
+    :param con_dict: authors's dictionary with screen name
+    :return: a dictionary with all new key_values pairs appended
+    """
+    user = api.get_user(screen_name=con_dict['author_screen_name'])
+    con_dict['verified'] = user.verified
+    con_dict['followers_count'] = user.followers_count
+    con_dict['listed_count'] = user.listed_count
+    return con_dict
+
+
+if __name__ == '__main__':
+    api = authenticate(token_file)
+    author_df = pd.read_csv(author_search, sep=',', error_bad_lines=False)
+    author_df = author_df.fillna('')
+    author_df_found = author_df.loc[author_df.author_screen_name != '', ]
+    con_info = author_df_found.T.to_dict().values()
+    new_con = list()
+    for i, c in enumerate(con_info):
+        if i % 500 == 0:
+            print(i)
+        c = fetch_user(api, c)
+        new_con.append(c)
+    df_new = pd.DataFrame.from_dict(new_con)
+    df_new['rep_score'] = np.log(df_new['followers_count'] + 1) / (
+                np.log(df_new['listed_count'] + 1) + 0.01)
+    df_new['rep_score_rank'] = df_new['rep_score'].rank()
+    df_new['followers_count_rank'] = df_new['followers_count'].rank(
+        ascending=False)
+    df_new['listed_count_rank'] = df_new['listed_count'].rank(ascending=False)
+    df_new.to_csv(author_twitter_rep, index=False)
